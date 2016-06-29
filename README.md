11ba2cbc4004498d1d8cdf1959d9e082b322b20f
xref_lee <xref_lee@yeah.net>
2016-06-29 14:31:55 +0800

    no message

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # XFLoopView
-新增 aaa
+###一个轮播图的 demo
+####实现原理:
+在一个 scrollview 上，用3张 imageView 实现轮播,左边 imageView、中间 imageView、右边 imageView。动画效果完成中间的 imageView 滑动到左边，右边的 imageView 滑动到中间。
+
+在这一时刻同时做两件事情：
+
+1.把右边的imageView 里的image赋值给中间的 imageView，而右边的 imageView 传入下一张 image。
+2.把中间的 imageView 放回屏幕中间，把右边的 imageView 放回屏幕右边右边，（改变 scrollview 的偏移量）
+
+####关于缓存：
+传入图片的方式有两种：
+
+imageArr：
+传入 image 或者 url，传入 url 是调用的 sdwebimage 实现的缓存。
+
+imageUrlArr：
+传入 url 自定义的缓存路径，存储到沙盒的 plist 里。
--- XFLoopView/XFLoopView/MyViewController.m
+++ XFLoopView/XFLoopView/MyViewController.m
@@ -22,9 +22,9 @@
     [super viewDidLoad];
 
     NSArray *urlArr = @[@"http://p1.qqyou.com/pic/UploadPic/2013-3/19/2013031923222781617.jpg",
-                     @"http://cdn.duitang.com/uploads/item/201409/27/20140927192649_NxVKT.thumb.700_0.png",
-                     @"http://img4.duitang.com/uploads/item/201409/27/20140927192458_GcRxV.jpeg",
-                     @"http://cdn.duitang.com/uploads/item/201304/20/20130420192413_TeRRP.thumb.700_0.jpeg"];
+                        @"http://cdn.duitang.com/uploads/item/201409/27/20140927192649_NxVKT.thumb.700_0.png",
+                        @"http://img4.duitang.com/uploads/item/201409/27/20140927192458_GcRxV.jpeg",
+                        @"http://cdn.duitang.com/uploads/item/201304/20/20130420192413_TeRRP.thumb.700_0.jpeg"];
     self.loopPlayView = [[XFLoopPlayView alloc]initWithFrame:CGRectMake(20, 100, 335, 200)];
     [self.view addSubview:self.loopPlayView];
     self.loopPlayView.imageArr = urlArr;
